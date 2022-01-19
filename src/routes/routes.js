722b1886d607350f8f1792871e8e558398ee3303
Bruno Barbosa <bruno.cbarbosa90@gmail.com>
2022-01-18 23:05:19 -0300

    getting users

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,7 +4,17 @@ const User = require('../models/userService')
 router.post('/', async (req, res) => {
   const {name, password, email} = req.body
 
-  if(!name || !password || !email) res.status(400).json({error: "Missing data!!"})
+  if(!name || !password || !email) {
+    res.status(400).json({error: "Missing data!!"})
+    return
+  }
+
+  const existsUser = await User.findOne({ email: email })
+
+  if(existsUser){
+    res.status(400).json({error: "Email exists"})
+    return
+  }
 
   const user = {
     name,
@@ -18,7 +28,35 @@ router.post('/', async (req, res) => {
 
     res.status(201).json({message: 'User created successful!!'})
 
+  } catch (error) {
+    res.status(500).json({error: error})
+  }
+})
 
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find()
+
+    res.status(200).json(users)
+
+  } catch (error) {
+    res.status(500).json({error: error})
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  const id = req.params.id
+
+  try {
+   
+    
+    if(!user){
+      console.log('entrou')
+      res.status(400).json({error: "User do not exists!!"})
+      return
+    }
+
+    res.status(200).json(user)
   } catch (error) {
     res.status(500).json({error: error})
   }
